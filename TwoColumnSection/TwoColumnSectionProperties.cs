b23b71cb26d6cf55acac9946abf6debf94857dea
Joshua Hess <jhess@hbs.net>
2025-06-03 18:43:40 -0500

    Updated to add the two column section

---
+++ TwoColumnSection/TwoColumnSectionProperties.cs
@@ -0,0 +1,17 @@
+﻿using BlockFarmEditor.Blocks;
+using BlockFarmEditor.RCL.Library.Attributes;
+using BlockFarmEditor.RCL.Models.BuilderModels;
+
+[assembly: BlockFarmEditorContainer("blockfarmeditor.section-2-columns", "2 Column Section", ViewPath = "~/Components/Blocks/TestSection.cshtml", Icon = "folder", PropertiesType = typeof(TwoColumnSectionProperties))]
+
+
+namespace BlockFarmEditor.Blocks
+{
+    public class TwoColumnSectionProperties : IBuilderProperties
+    {
+        //[BlockFarmEditorPropertyEditor(Constants.PropertyEditors.Aliases.RichText, "Rich Text")]
+        //[BlockFarmEditorPropertyEditorConfig(typeof(RichTextEditorConfig))]
+        [BlockFarmEditorDataType("Textstring", "Header")]
+        public string Header { get; set; } = string.Empty;
+    }
+}
